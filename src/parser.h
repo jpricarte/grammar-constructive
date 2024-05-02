fd716d68a80bbe93cb115a3c8309fdd4eb73ca62
Jordi Pujol <jpricarte@inf.ufrgs.br>
2024-05-02 13:09:43 -0300

    Create struct for Algorithm Configuration - enhancement #2

--- src/parser.h
+++ src/parser.h
@@ -1,6 +1,20 @@
 #pragma once
+
+#include <functional>
+#include "ConstructiveAlgorithm.h"
+#include "Problem.h"
 #include "../submodule/json/single_include/nlohmann/json.hpp"
 
-struct AlgorithmConfiguration {
+class AlgorithmConfiguration {
 	
+	nlohmann::json configuration;
+	
+	std::function<problem::SolutionPtr(problem::Problem&, problem::Instance&)> algorithm;
+	std::function<problem::ElementPtr(problem::Instance&, problem::SolutionPtr)> elementselection;
+	std::function<int(int)> random_distribuiton;
+
+	public:
+		AlgorithmConfiguration() = default;
+
+		void loadConfiguration(std::string path);
 };
